cb8747729aad5593d854dd6b4d18a089e97677de
Daniel Fortich <fortichdaniel16@gmail.com>
2025-01-11 13:00:02 -0500

    bank account logger implementation

--- dfm18/bank_account.py
+++ dfm18/bank_account.py
@@ -1,15 +1,39 @@
+from typing import Optional
+
+import logging
+
+
 class BankAccount:
-    def __init__(self, balance: float = 0):
+    def __init__(self, balance: float = 0, log_file: Optional[str] = None):
         self._balance = balance
+        self.log_file = log_file
+        self._setup_logger()
+
+    def _setup_logger(self):
+        self._logger = logging.getLogger(__name__)
+        self._logger.setLevel(logging.INFO)
+
+        if self.log_file is not None:
+            file_handler = logging.FileHandler(self.log_file)
+            file_handler.setLevel(logging.INFO)
+
+            formatter = logging.Formatter(
+                "%(asctime)s - %(message)s", datefmt="%b %d, %Y %H:%M:%S"
+            )
+            file_handler.setFormatter(formatter)
+
+            self._logger.addHandler(file_handler)
 
     def deposit(self, amount: float) -> float:
         if amount > 0:
             self._balance += amount
+            self._logger.info("Deposited %.2f. New balance: %.2f", amount, self.balance)
         return self._balance
 
     def withdraw(self, amount: float) -> float:
         if amount > 0:
             self._balance -= amount
+            self._logger.info("Withdrew %.2f. New balance: %.2f", amount, self.balance)
         return self._balance
 
     @property
--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -1,11 +1,20 @@
 import unittest
 
+import os
+
 from dfm18.bank_account import BankAccount
 
 
 class TestBankAccount(unittest.TestCase):
     def setUp(self):
-        self.account = BankAccount(balance=1000)
+        self.account = BankAccount(balance=1000, log_file="transaction.log")
+
+    def tearDown(self):
+        for handler in self.account._logger.handlers:
+            handler.close()
+
+        if os.path.exists(self.account.log_file):
+            os.remove(self.account.log_file)
 
     def test_deposit(self):
         new_balance = self.account.deposit(500)
@@ -21,3 +30,10 @@ class TestBankAccount(unittest.TestCase):
         self.assertEqual(self.account.balance, 500)
         self.account.deposit(100)
         self.assertEqual(self.account.balance, 600)
+
+    def test_logger(self):
+        with self.assertLogs(self.account._logger) as log_file:
+            self.account.deposit(1000)
+            self.assertIn("Deposited 1000.00. New balance: 2000.00", log_file.output[0])
+            self.account.withdraw(50)
+            self.assertIn("Withdrew 50.00. New balance: 1950.00", log_file.output[1])
